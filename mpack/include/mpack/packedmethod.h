02e0e83d2284817d9a5aefd712c7af5b5894b8d4
ousttrue <ousttrue@gmail.com>
2014-02-18 11:57:34 +0900

    refactoring. pack_internal

--- mpack/include/mpack/packedmethod.h
+++ mpack/include/mpack/packedmethod.h
@@ -7,41 +7,41 @@ namespace msgpack {
 
 	typedef std::function<void(packer&, unpacker&)> packedmethod;
 
+    // build packed method call
+    template<typename F, typename R, typename A1, typename A2>
+        packedmethod pack_internal(F &f)
+        {
+            return [f](packer &p, unpacker &u){
+                // unpack args
+                A1 a1; A2 a2;
+                u >> a1 >> a2;
+                // call
+                R r=f(a1, a2);
+                // pack result
+                p << r;
+            };
+        }
+
     // for function pointer
     template<typename R, typename A1, typename A2>
         packedmethod packmethod(R(*f)(A1,A2))
         {
-            return [f](packer &p, unpacker &u){
-                // unpack args
-                A1 a1; A2 a2;
-                u >> a1 >> a2;
-                // call
-                R r=f(a1, a2);
-                // pack result
-                p << r;
-            };
+			typedef R(*F)(A1, A2);
+            return pack_internal<F, R, A1, A2>(f);
         }
 
-    // for functor 2
+    // extract arguments and return
     template<typename F, typename C, typename R, typename A1, typename A2>
-        packedmethod _packmethod(F &f, R(C::*)(A1,A2)const)
+        packedmethod pack_functor(F &f, R(C::*)(A1,A2)const)
         {
-            return [f](packer &p, unpacker &u){
-                // unpack args
-                A1 a1; A2 a2;
-                u >> a1 >> a2;
-                // call
-                R r=f(a1, a2);
-                // pack result
-                p << r;
-            };
+            return pack_internal<F, R, A1, A2>(f);
         }
 
-    // for functor 1
+    // for functor
     template<typename F>
         packedmethod packmethod(F &f)
         {
-            return _packmethod(f, &F::operator());
+            return pack_functor(f, &F::operator());
         }
 
 } // namespace
