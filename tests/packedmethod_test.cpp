2715fd884d41ca8550b54018aa1ddd41a4221235
ousttrue <ousttrue@gmail.com>
2014-02-18 19:45:14 +0900

    add test

--- tests/packedmethod_test.cpp
+++ tests/packedmethod_test.cpp
@@ -18,6 +18,47 @@ static int bin(int a, int b)
 }
 
 
+TEST(PackedMethod, fp_0to1) 
+{
+    // wrap static function pointer
+    auto method=mpack::msgpack::packmethod(zero);
+
+    // call method(packing result to result_packer)
+    auto result_packer=mpack::msgpack::create_vector_packer();
+    method(result_packer, 
+            mpack::msgpack::create_unpacker_from_packer(
+                mpack::msgpack::create_vector_packer() 
+                << mpack::msgpack::array(0) 
+                ));
+
+    // unpack result
+	auto u = mpack::msgpack::create_unpacker_from_packer(result_packer);
+    int n;
+    u >> n;
+    EXPECT_EQ(0, n);
+}
+
+TEST(PackedMethod, fp_1to1) 
+{
+    // wrap static function pointer
+    auto method=mpack::msgpack::packmethod(one);
+
+    // call method(packing result to result_packer)
+    auto result_packer=mpack::msgpack::create_vector_packer();
+    method(result_packer, 
+            mpack::msgpack::create_unpacker_from_packer(
+                mpack::msgpack::create_vector_packer() 
+                << mpack::msgpack::array(1) 
+                << 1
+                ));
+
+    // unpack result
+	auto u = mpack::msgpack::create_unpacker_from_packer(result_packer);
+    int n;
+    u >> n;
+    EXPECT_EQ(2, n);
+}
+
 TEST(PackedMethod, fp_2to1) 
 {
     // wrap static function pointer
@@ -44,17 +85,17 @@ TEST(PackedMethod, lambda_2to1)
     auto add_lambda=[](int a, int b){ return a+b; };
     auto method=mpack::msgpack::packmethod(add_lambda);
 
-    // pack args 
-    auto args_packer=mpack::msgpack::create_vector_packer();
-    args_packer << mpack::msgpack::array(2) << 1 << 2;
-
     // call method(packing result to result_packer)
     auto result_packer=mpack::msgpack::create_vector_packer();
-	auto args_unpacker = mpack::msgpack::create_memory_unpacker (args_packer.pointer(), args_packer.size());
-    method(result_packer, args_unpacker);
+    method(result_packer, 
+            mpack::msgpack::create_unpacker_from_packer(
+                mpack::msgpack::create_vector_packer() 
+                << mpack::msgpack::array(2) 
+                << 1 << 2
+                ));
 
     // unpack result
-	auto u = mpack::msgpack::create_memory_unpacker(result_packer.pointer(), result_packer.size());
+	auto u = mpack::msgpack::create_unpacker_from_packer(result_packer);
     int n;
     u >> n;
     EXPECT_EQ(3, n);
