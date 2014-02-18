0d94bc6a7460d0c0e81f1facb0964b3383919aff
ousttrue <ousttrue@gmail.com>
2014-02-18 11:48:47 +0900

    add lambda_2to1

--- mpack/include/mpack/packedmethod.h
+++ mpack/include/mpack/packedmethod.h
@@ -22,9 +22,9 @@ namespace msgpack {
             };
         }
 
-    // for functor
-    template<typename R, typename A1, typename A2>
-        packedmethod packmethod(std::function<R(A1,A2)> &f)
+    // for functor 2
+    template<typename F, typename C, typename R, typename A1, typename A2>
+        packedmethod _packmethod(F &f, R(C::*)(A1,A2)const)
         {
             return [f](packer &p, unpacker &u){
                 // unpack args
@@ -35,6 +35,13 @@ namespace msgpack {
                 // pack result
                 p << r;
             };
+        }
+
+    // for functor 1
+    template<typename F>
+        packedmethod packmethod(F &f)
+        {
+            return _packmethod(f, &F::operator());
         }
 
 } // namespace
--- tests/packedmethod_test.cpp
+++ tests/packedmethod_test.cpp
@@ -6,10 +6,32 @@ static int add(int a, int b)
     return a+b;
 }
 
-TEST(PackedMethod, method2to1) 
+TEST(PackedMethod, fp_2to1) 
 {
     // wrap static function pointer
     auto method=mpack::msgpack::packmethod(add);
+    auto add_lambda=[](int a, int b){ return a+b; };
+
+    // pack args 
+    auto args_packer=mpack::msgpack::create_vector_packer();
+    args_packer << 1 << 2;
+
+    // call method(packing result to result_packer)
+    auto result_packer=mpack::msgpack::create_vector_packer();
+	auto args_unpacker = mpack::msgpack::create_memory_unpacker (args_packer.pointer(), args_packer.size());
+    method(result_packer, args_unpacker);
+
+    // unpack result
+	auto u = mpack::msgpack::create_memory_unpacker(result_packer.pointer(), result_packer.size());
+    int n;
+    u >> n;
+    EXPECT_EQ(3, n);
+}
+
+TEST(PackedMethod, lambda_2to1) 
+{
+    auto add_lambda=[](int a, int b){ return a+b; };
+    auto method=mpack::msgpack::packmethod(add_lambda);
 
     // pack args 
     auto args_packer=mpack::msgpack::create_vector_packer();
