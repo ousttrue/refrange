eebdab3f63e55e8637132cef7cb2fdd7c955d379
ousttrue <ousttrue@gmail.com>
2014-02-25 01:08:29 +0900

    implement vpd loader

--- refrange/include/refrange/text/vpd.h
+++ refrange/include/refrange/text/vpd.h
@@ -6,9 +6,39 @@ namespace refrange {
 namespace text {
 namespace vpd {
 
+
+struct vec3
+{
+    float x;
+    float y;
+    float z;
+};
+
+
+struct quaternaion
+{
+    float x;
+    float y;
+    float z;
+    float w;
+};
+
+
+struct bonetransformation
+{
+    std::string name;
+    vec3 translation;
+    quaternaion rotation;
+};
+
+
 class loader
 {
+    std::vector<bonetransformation> m_bones;
+
 public:
+    std::vector<bonetransformation> &get_bones(){ return m_bones; }
+
     bool load(const immutable_range &r)
     {
         line_reader reader(r);
@@ -17,17 +47,57 @@ public:
             return false;
 		}
 
-		{
-			auto line = reader.get_line(); //miku.osm;		// 親ファイル名
-			int a = 0;
-		}
+        reader.get_line(); //miku.osm;
 
-		{
-			auto line = reader.get_line();
-			int bonenum = line.to_int();
-		}
+        // bones
+		int bonenum = reader.get_line().to_int();
 
-        return false;
+        for(int i=0; i<bonenum; ++i){
+            m_bones.push_back(bonetransformation());
+            auto &bone=m_bones.back();
+
+            // name
+            {
+                auto line=reader.get_line();
+                auto splited=line.split('{');
+                auto it=splited.begin();
+                ++it;
+                bone.name=it->to_str();
+            }
+
+            // translation
+            {
+                auto line=reader.get_line();
+                auto splited=line.split();
+                auto it=splited.begin();
+                bone.translation.x=it->to_float();
+                ++it;
+                bone.translation.y=it->to_float();
+                ++it;
+                bone.translation.z=it->to_float();
+            }
+
+            // rotation
+            {
+                auto line=reader.get_line();
+                auto splited=line.split();
+                auto it=splited.begin();
+                bone.rotation.x=it->to_float();
+                ++it;
+                bone.rotation.y=it->to_float();
+                ++it;
+                bone.rotation.z=it->to_float();
+                ++it;
+                bone.rotation.w=it->to_float();
+            }
+
+            {
+                auto line=reader.get_line();
+                assert(line=="}");
+            }
+        }
+
+        return true;
     }
 };
 
--- tests/vpd_test.cpp
+++ tests/vpd_test.cpp
@@ -7,6 +7,8 @@ TEST(VpdTest, loader)
     auto path="../../samples/sample.vpd";
     auto buf=refrange::readfile(path);
     refrange::text::vpd::loader vpd;
-    //ASSERT_TRUE(vpd.load(refrange::vectorrange(buf)));
+    ASSERT_TRUE(vpd.load(refrange::vectorrange(buf)));
+
+	return;
 }
 
