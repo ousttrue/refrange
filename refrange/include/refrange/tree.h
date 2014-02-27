689042eeca130fc71cde14c1ed030b5c26d5e21f
ousttrue <ousttrue@gmail.com>
2014-02-27 20:22:10 +0900

    update refrange::tree

--- refrange/include/refrange/tree.h
+++ refrange/include/refrange/tree.h
@@ -9,10 +9,41 @@ template<typename T>
     {
         T value;
 		std::list<node<T>> children;
+        node<T> *parent;
+
+        node()
+            : parent(0)
+        {
+        }
+
+        node(const T &t)
+            : value(t), parent(0)
+        {
+        }
 
         bool operator==(const node<T> &rhs)const
         {
             return value==rhs.value && children==rhs.children;
+        }
+
+        node<T> *find(const T &key)
+        {
+            if(value==key){
+                return this;
+            }
+            for(auto it=children.begin(); it!=children.end(); ++it){
+                auto found=it->find(key);
+                if(found){
+                    return found;
+                }
+            }
+            return 0;
+        }
+
+        void add_child(node<T> &child)
+        {
+            child.parent=this;
+            children.push_back(child);
         }
     };
 
